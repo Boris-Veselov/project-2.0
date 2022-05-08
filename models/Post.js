0ad6f148730956e711363f0723494947e535f677
Michael Phan <michaelphan35@gmail.com>
2022-05-08 00:24:31 -0700

    created post model

---
+++ models/Post.js
@@ -0,0 +1,45 @@
+const {Model, DataTypes} = require('sequelize');
+const { model } = require('../config/connection');
+const sequelize = require('../config/connection');
+
+class Post extends Model {}
+
+Post.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true
+        },
+
+        title: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+
+        post_content: {
+            type: DataTypes.TEXT,
+            allowNull: true
+        },
+
+        user_id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            references: {
+                model: 'user',
+                key: 'id'
+            }
+        }
+    },
+
+    {
+        sequelize,
+        freezeTableName: true,
+        undeerscored: true,
+        modelName: 'post'
+    }
+
+);
+
+model.exports = Post;
