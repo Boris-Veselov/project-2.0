46489e332690a69d3c31a55f688a90f13d0493d5
mmilner29 <madisonm4514@gmail.com>
2022-05-08 22:13:05 -0700

    add preview & comments

--- public/javascript/main.js
+++ public/javascript/main.js
@@ -1,9 +1,12 @@
 var granimInstance = new Granim({
+    //selects gradient element
     element: '#Granim',
+    //type of gradient
     direction: 'diagonal',
     isPausedWhenNotInView: true,
     states : {
         "default-state": {
+            //gradient colors
             gradients: [
                 ['#ff9966', '#ff5e62'],
                 ['#00F260', '#0575E6'],
