50a0f769746847b19d5fa7c7b099746d724ccdb3
Boris Veselov <borisveselov@yahoo.com>
2022-05-10 20:38:46 -0400

    heroku updates

--- server.js
+++ server.js
@@ -1,28 +1,28 @@
 const express = require('express');
 // const sequelize = require('./config/connection');
-// const path = require('path');
+const path = require('path');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
 const router = require('express').Router();
 
-app.get('/', (req, res) => {
-  res.send('home');
-});
+// app.get('/', (req, res) => {
+//   res.send('home');
+// });
 
-// const exphbs = require('express-handlebars');
-// const hbs = exphbs.create({});
+const exphbs = require('express-handlebars');
+const hbs = exphbs.create({});
 
-// app.engine('handlebars', hbs.engine);
-// app.set('view engine', 'handlebars');
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
 
-// app.use(express.json());
-// app.use(express.urlencoded({ extended: true }));
-// app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
 
 // turn on controllers
-// app.use(require('./controllers/'));
+app.use(require('./controllers/'));
 
 
 // sync sequelize models to the database, then turn on the server
