4fa6e27d4d785c52f56c13ae067abe0e6ba9eb11
Boris Veselov <borisveselov@yahoo.com>
2022-05-10 20:18:52 -0400

    comment out

--- server.js
+++ server.js
@@ -1,25 +1,25 @@
 const express = require('express');
-const sequelize = require('./config/connection');
-const path = require('path');
+// const sequelize = require('./config/connection');
+// const path = require('path');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-const exphbs = require('express-handlebars');
-const hbs = exphbs.create({});
+// const exphbs = require('express-handlebars');
+// const hbs = exphbs.create({});
 
-app.engine('handlebars', hbs.engine);
-app.set('view engine', 'handlebars');
+// app.engine('handlebars', hbs.engine);
+// app.set('view engine', 'handlebars');
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, 'public')));
+// app.use(express.json());
+// app.use(express.urlencoded({ extended: true }));
+// app.use(express.static(path.join(__dirname, 'public')));
 
 // turn on controllers
-app.use(require('./controllers/'));
+// app.use(require('./controllers/'));
 
 
 // sync sequelize models to the database, then turn on the server
-sequelize.sync({ force: false }).then(() => {
-  app.listen(PORT, () => console.log('Now listening'));
-});
+// sequelize.sync({ force: false }).then(() => {
+  app.listen(process.env.PORT, () => console.log('Now listening'));
+// });
