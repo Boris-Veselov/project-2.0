99028f7a1d0cb986648119f08e38c733ec49fb45
Michael Phan <michaelphan35@gmail.com>
2022-05-11 17:17:04 -0700

    slight tweaks

--- controllers/api/post-route.js
+++ controllers/api/post-route.js
@@ -3,6 +3,22 @@ const {Post, User, Comment} = require ('../../models');
 const sequelize = require('../../config/connection');
 const auth = require('../../utils/auth')
 
+router.post('/', (req, res) => {
+    if (req.session) {
+        Post.create({
+            title: req.body.title,
+            post_content: req.body.post_content,
+            user_id: req.session.user_id
+        })
+            .then(dbPostData => res.json(dbPostData))
+            .catch(err => {
+                console.log(err);
+                res.status(500).json(err);
+            });
+    }
+
+});
+
 router.get('/', (req, res) => {
     Post.findAll({
         attributes: [
@@ -30,7 +46,7 @@ router.get('/', (req, res) => {
             console.log(err);
             res.status(500).json(err);
         });
-})
+});
 
 router.get('/:id', (req, res) => {
     Post.findOne({
@@ -39,8 +55,8 @@ router.get('/:id', (req, res) => {
         },
         attributes: [
             'id',
-            'post_content',
             'title',
+            'post_content',
         ],
         include: [
             {
@@ -61,24 +77,6 @@ router.get('/:id', (req, res) => {
             res.status(500).json(err);
         });
 });
-
-router.post('/', (req, res) => {
-    if (req.session) {
-        Post.create({
-            title: req.body.title,
-            post_content: req.body.post_content,
-            user_id: req.session.user_id
-        })
-            .then(dbPostData => res.json(dbPostData))
-            .catch(err => {
-                console.log(err);
-                res.status(500).json(err);
-            });
-    }
-
-})
-
-
 
 router.put('/:id', auth, (req, res) => {
     Post.update(
